3552769f916f63561940168540e382c875f92f6e
Michal Wrzesniewski <wrzesniewski.michal@gmail.com>
2019-02-02 22:51:18 +0100

    Change

--- CameraFileCopier2/Program.cs
+++ CameraFileCopier2/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Windows.Forms;
 
 namespace CameraFileCopier2
 {
@@ -11,27 +10,34 @@ namespace CameraFileCopier2
         [STAThread]
         static void Main(string[] args)
         {
-            string pathToCopyFrom = Clipboard.GetText();
-            Console.WriteLine(pathToCopyFrom);
-            Console.WriteLine("Continue? (Y/N)");
-            ConsoleKeyInfo key = Console.ReadKey();
-            if (key.Key != ConsoleKey.Y)
-            {
-                return;
-            }
+            Console.WriteLine("Give your start path...");
+            string pathToCopyFrom = Console.ReadLine();
             Console.WriteLine();
-            List<string> files = Directory.GetFiles(pathToCopyFrom).ToList();
+            List<string> files = Directory.GetFiles(pathToCopyFrom, "*", SearchOption.AllDirectories).ToList();
+            string destinationDirectory = Path.Combine(pathToCopyFrom, "renamed");
             Dictionary<string, string> renamed = new Dictionary<string, string>();
             foreach (string file in files)
             {
+                if (Path.GetDirectoryName(file).Contains(destinationDirectory))
+                {
+                    continue;
+                }
                 string oldName = Path.GetFileName(file);
-                DateTime creationTime = File.GetLastWriteTime(file);
-                string newName = string.Format("{0:yyyy-MM-dd HH-mm-ss}_{1}", creationTime, oldName);
-                renamed.Add(oldName, newName);
+                string extension = Path.GetExtension(file);
+                bool isJpgFile = extension.Equals(".jpg", StringComparison.InvariantCultureIgnoreCase);
+                bool isMtsFile = extension.Equals(".mts", StringComparison.InvariantCultureIgnoreCase);
+                if (!isJpgFile && !isMtsFile)
+                {
+                    continue;
+                }
+
+                DateTime timeStamp = File.GetLastWriteTime(file);
+                string newName = string.Format("{0:yyyy-MM-dd HH-mm-ss}_{1}", timeStamp, oldName);
+                renamed.Add(file, Path.Combine(destinationDirectory, newName));
             }
             foreach (KeyValuePair<string, string> item in renamed)
             {
-                Console.WriteLine(string.Format("{0}{1}{2}", item.Key, " ---> ", item.Value));
+                Console.WriteLine(string.Format("{0}{1}{2}", Path.GetFileName(item.Key), " ---> ", Path.GetFileName(item.Value)));
             }
             Console.WriteLine("Rename? (Y/N)");
             if (Console.ReadKey().Key != ConsoleKey.Y)
@@ -39,9 +45,14 @@ namespace CameraFileCopier2
                 return;
             }
             Console.WriteLine();
+            
+            if (!Directory.Exists(destinationDirectory))
+            {
+                Directory.CreateDirectory(destinationDirectory);
+            }
             foreach (KeyValuePair<string, string> item in renamed)
             {
-                File.Move(Path.Combine(pathToCopyFrom, item.Key), Path.Combine(pathToCopyFrom, item.Value));
+                File.Move(item.Key, item.Value);
                 Console.WriteLine(string.Format("{0}{1}{2}{3}", item.Key, " ---> ", item.Value, " ----> OK!"));
             }
 
